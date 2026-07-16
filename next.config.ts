8b3ed1bf6f59fe64ba4a462f28d931263a40f8b9
fmarquez <fmarquez@fgs.co>
2026-07-15 19:07:56 -0500

    feat:Configurar, despliegue en GitHubPages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+const repositoryName = "medioevofest";
+
+const nextConfig: NextConfig = {
+  output: "export",
+
+  basePath: isGitHubPages ? `/${repositoryName}` : "",
+
+  assetPrefix: isGitHubPages ? `/${repositoryName}/` : "",
+
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
